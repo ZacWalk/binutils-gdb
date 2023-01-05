06c156c7fc5796c5c3d26bafa8f5cd14491a2028
Evgeny Karpov <eukarpov@gmail.com>
2023-01-05 18:16:13 +0100

    * add sync fork action

---
+++ .github/workflows/scripts/sync-fork.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex # stop bash script on error
+
+git remote add upstream https://github.com/bminor/binutils-gdb.git
+git fetch upstream
+git checkout master
+git merge upstream/master
+git push
