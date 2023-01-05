ebb358dc451b79883f7abd7550cde2ec4a5fcb55
Evgeny Karpov <eukarpov@gmail.com>
2023-01-05 18:39:52 +0100

    * fix destination branch name * fix filesystem permissions

--- .github/workflows/scripts/sync-fork.sh
+++ .github/workflows/scripts/sync-fork.sh
@@ -4,6 +4,6 @@ set -ex # stop bash script on error
 
 git remote add upstream https://github.com/bminor/binutils-gdb.git
 git fetch upstream
-git checkout master
+git checkout origin/master
 git merge upstream/master
 git push
