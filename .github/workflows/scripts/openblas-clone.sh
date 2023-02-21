d02db6280a55563a551767638bc07a5a6a96374c
Nursultan Zarlyk <nzarlyk@microsoft.com>
2023-02-21 16:06:13 +0100

    Pin openblas

--- .github/workflows/scripts/openblas-clone.sh
+++ .github/workflows/scripts/openblas-clone.sh
@@ -2,7 +2,8 @@
 
 set -ex # stop bash script on error
 
-git clone --depth 1 https://github.com/xianyi/OpenBLAS.git openblas
+git clone https://github.com/xianyi/OpenBLAS.git openblas
 cd openblas
+git checkout 10be02c89605334b3095c0ac76f4aa328ae54f72
 sed -i '143,145d' ./common_arm64.h 
 mkdir build
