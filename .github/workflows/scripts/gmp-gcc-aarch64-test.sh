a03de684b43419f42cc2d50f056ffa506752c829
Nursultan Zarlyk <nzarlyk@microsoft.com>
2023-02-21 15:36:30 +0100

    Fix vcpkg libtool version

--- .github/workflows/scripts/gmp-gcc-aarch64-test.sh
+++ .github/workflows/scripts/gmp-gcc-aarch64-test.sh
@@ -36,8 +36,8 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 git checkout e46521db0 
 
-mkdir downloads
-cp ../.github/workflows/tools/msys-libtool-2.4.6-9-x86_64.pkg.tar.xz ./downloads
+sed -i '187s/.*/URL "https:\/\/repo.msys2.org\/msys\/x86_64\/libtool-2.4.6-14-x86_64.pkg.tar.zst"/' scripts/cmake/vcpkg_acquire_msys.cmake
+sed -i '188s/.*/SHA512 ba983ed9c2996d06b0d21b8fab9505267115f2106341f130e92d6b66dad87b0f0e82707daf0b676a28966bfaa24f6c41b6eef9e1f9bf985611471024f2b0ac97/' scripts/cmake/vcpkg_acquire_msys.cmake
 
 cat ../.github/workflows/patches/gmp-gcc-vcpkg.patch | sed -e "s/^++runner_workspace=/++runner_workspace=\"${workspace_folder//\//\\/}\"/" | git apply --whitespace=fix
 ./bootstrap-vcpkg.sh
