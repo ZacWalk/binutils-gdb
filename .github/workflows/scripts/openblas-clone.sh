917789708d330c9c2b43677b0d525a025c33ac02
Nursultan Zarlyk <nzarlyk@microsoft.com>
2023-02-27 16:49:37 +0100

    There is no false in C... Remove openblas-clone.sh

--- .github/workflows/scripts/openblas-clone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex # stop bash script on error
-
-git clone https://github.com/xianyi/OpenBLAS.git openblas
-cd openblas
-git checkout 10be02c89605334b3095c0ac76f4aa328ae54f72
-sed -i '143,145d' ./common_arm64.h 
-mkdir build
