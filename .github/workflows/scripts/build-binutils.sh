b2b9e9baff4406917897eb80cbc7406a848e5ee7
Nursultan Zarlyk <nzarlyk@microsoft.com>
2023-03-15 11:56:41 +0100

    Fix by using github.ref

--- .github/workflows/scripts/build-binutils.sh
+++ .github/workflows/scripts/build-binutils.sh
@@ -3,31 +3,28 @@
 set -ex # stop bash script on error
 . .github/workflows/scripts/base.sh
 
-echo $ci_merge_with_branch
-echo $ci_test_variable
-
-# ssh -i $gcc_identity $gcc_destination 'bash -sx' << ENDSSH
-#     set -e # stop bash script on error
-#     pwd
-#     [ -d $workspace_folder ] && rm -r $workspace_folder
-#     find $workspace_runner_folder -mindepth 1 -maxdepth 1 -type d -mmin +600 -exec rm -rf {} \; # clean up session files after 10h
-#     mkdir -p $workspace_folder
-#     cd $workspace_folder
-#     sudo apt-get update
-#     sudo apt-get -y install build-essential binutils-for-build texinfo bison flex zlib1g-dev libgmp-dev dejagnu libmpfr-dev
-#     git clone ${gcc_remote_url}
-#     cd binutils-gdb
-#     git fetch origin $rev_reference:pullrequest
-#     git checkout pullrequest
-#     echo $ci_merge_with_branch
-#     if [ ! -z "$ci_merge_with_branch" ]; then
-#         git fetch origin $ci_merge_with_branch:$ci_merge_with_branch
-#         git config user.name github-actions
-#         git config user.email github-actions@github.com
-#         git merge $ci_merge_with_branch --no-commit
-#     fi
-#     mkdir -p $gcc_build_folder
-#     cd $gcc_build_folder
-#     $gcc_source_folder/configure --target=aarch64-pe --prefix="\$HOME/cross"
-#     make -j\$(nproc)
-# ENDSSH
+ssh -i $gcc_identity $gcc_destination 'bash -sx' << ENDSSH
+    set -e # stop bash script on error
+    pwd
+    [ -d $workspace_folder ] && rm -r $workspace_folder
+    find $workspace_runner_folder -mindepth 1 -maxdepth 1 -type d -mmin +600 -exec rm -rf {} \; # clean up session files after 10h
+    mkdir -p $workspace_folder
+    cd $workspace_folder
+    sudo apt-get update
+    sudo apt-get -y install build-essential binutils-for-build texinfo bison flex zlib1g-dev libgmp-dev dejagnu libmpfr-dev
+    git clone ${gcc_remote_url}
+    cd binutils-gdb
+    git fetch origin $rev_reference:pullrequest
+    git checkout pullrequest
+    echo $ci_merge_with_branch
+    if [ ! -z "$ci_merge_with_branch" ]; then
+        git fetch origin $ci_merge_with_branch:$ci_merge_with_branch
+        git config user.name github-actions
+        git config user.email github-actions@github.com
+        git merge $ci_merge_with_branch --no-commit
+    fi
+    mkdir -p $gcc_build_folder
+    cd $gcc_build_folder
+    $gcc_source_folder/configure --target=aarch64-pe --prefix="\$HOME/cross"
+    make -j\$(nproc)
+ENDSSH
