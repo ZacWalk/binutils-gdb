094b0189c622524c739bfda6055e113f9a813dd5
Nursultan Zarlyk <nzarlyk@microsoft.com>
2023-04-12 18:19:59 +0200

    Fix VCPKG msys2-runtime version

--- .github/workflows/scripts/gmp-gcc-aarch64-test.sh
+++ .github/workflows/scripts/gmp-gcc-aarch64-test.sh
@@ -12,8 +12,8 @@ git checkout e46521db0
 sed -i '187s/.*/URL "https:\/\/repo.msys2.org\/msys\/x86_64\/libtool-2.4.6-14-x86_64.pkg.tar.zst"/' scripts/cmake/vcpkg_acquire_msys.cmake
 sed -i '188s/.*/SHA512 ba983ed9c2996d06b0d21b8fab9505267115f2106341f130e92d6b66dad87b0f0e82707daf0b676a28966bfaa24f6c41b6eef9e1f9bf985611471024f2b0ac97/' scripts/cmake/vcpkg_acquire_msys.cmake
 
-sed -i '346s/.*/URL "https:\/\/repo.msys2.org\/msys\/x86_64\/msys2-runtime-3.2.0-13-x86_64.pkg.tar.zst"/' scripts/cmake/vcpkg_acquire_msys.cmake
-sed -i '347s/.*/SHA512 969a1dbb570ff5636fb16836ccbcd82d5d929eb272c65c4b2298800fb8e1b664d73f073c4a286367a9d124d9a5b1202dc63dcd1a1a901b868834c99e111f8604/' scripts/cmake/vcpkg_acquire_msys.cmake
+sed -i '346s/.*/URL "https:\/\/repo.msys2.org\/msys\/x86_64\/msys2-runtime-3.2.0-15-x86_64.pkg.tar.zst"/' scripts/cmake/vcpkg_acquire_msys.cmake
+sed -i '347s/.*/SHA512 e054f5e1fc87add7fc3f5cbafd0ff5f3f77a30385073c17d0a8882feb3319a4cba355ccb1cb67ccb9fa704b514c3d05f54c45199011f604c69bb70e52fa33455/' scripts/cmake/vcpkg_acquire_msys.cmake
 
 cat ../.github/workflows/patches/gmp-gcc-vcpkg.patch | sed -e "s/^++runner_workspace=/++runner_workspace=\"${workspace_folder//\//\\/}\"/" | git apply --whitespace=fix
 ./bootstrap-vcpkg.sh
